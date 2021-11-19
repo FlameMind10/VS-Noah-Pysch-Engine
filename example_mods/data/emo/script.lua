7392f153633f9e6a0a20491625874b4771623f81
MrFlare10 <flamemind10@gmail.com>
2021-11-19 04:28:42 -0600

    Initial commit

---
+++ example_mods/data/emo/script.lua
@@ -0,0 +1,103 @@
+local allowCountdown = false
+local xx = 440;
+local yy = 500;
+local xx2 = 870;
+local yy2 = 500;
+local ofs = 30;
+local swag = 2;
+local followchars = true;
+function onStartCountdown()
+	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
+	    setProperty('inCutscene', true);
+		startVideo('noahcut');
+		runTimer('startDialogue', 18);
+		allowCountdown = true;
+		return Function_Stop;
+	end
+	return Function_Continue;
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	if tag == 'startDialogue' then -- Timer completed, play dialogue
+		startDialogue('dialogue');
+	end
+end
+
+function onUpdate()
+if curStep == 636 then
+swag = 1;
+end
+if curStep == 895 then
+swag = 2;
+end
+ if swag == 1 then
+ setProperty('defaultCamZoom',0.9)
+ end
+ if swag == 2 then
+ setProperty('defaultCamZoom',0.8)
+ end
+ 
+    if followchars == true then
+        if mustHitSection == false then
+            
+            if getProperty('dad.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singLEFT-alt' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singRIGHT-alt' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singUP-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singDOWN-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+        else
+            
+            if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx2-ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx2+ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx2,yy2-ofs)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx2,yy2+ofs)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'idle-alt' then
+                triggerEvent('Camera Follow Pos',xx2,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx2,yy2)
+            end
+        end
+    else
+        triggerEvent('Camera Follow Pos','','')
+    end
+    ---local currentBeat = (getSongPosition() / 1000)*(bpm/60)
+    --for i = 0, 8 do
+        --setPropertyFromGroup('playerStrums', i, 'y', _G['defaultPlayerStrumY'..i] + 124 * math.sin((currentBeat + i*2) * math.pi), i)
+        --setPropertyFromGroup('opponentStrums', i, 'x', _G['defaultPlayerStrumX'..i] + 64 * math.sin((currentBeat + i*2) * math.pi), i)
+    --end
+end
+
---
+++ example_mods/data/good-one/script.lua
@@ -0,0 +1,42 @@
+local allowCountdown = false
+function onStartCountdown()
+	-- Block the first countdown and start a timer of 0.8 seconds to play the dialogue
+	if not allowCountdown and isStoryMode and not seenCutscene then
+		setProperty('inCutscene', true);
+		runTimer('startDialogue', 0.8);
+		allowCountdown = true;
+		return Function_Stop;
+	end
+	return Function_Continue;
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	if tag == 'startDialogue' then -- Timer completed, play dialogue
+		startDialogue('dialogue');
+	end
+	if tag == 'endDialogue' then -- Timer completed, play dialogue
+		startDialogue('finaldialogue');
+	end
+end
+
+-- Dialogue (When a dialogue is finished, it calls startCountdown again)
+function onNextDialogue(count)
+	-- triggered when the next dialogue line starts, 'line' starts with 1
+end
+
+local seenCutscene = false;
+
+function onEndSong()
+if not seenCutscene and isStoryMode then
+		setProperty('inCutscene', true);
+		runTimer('endDialogue', 0.8);
+		seenCutscene = true;
+		return Function_Stop;
+		end		
+	return Function_Continue;
+end
+
+function onSkipDialogue(count)
+	-- triggered when you press Enter and skip a dialogue line that was still being typed, dialogue line starts with 1
+end
+
